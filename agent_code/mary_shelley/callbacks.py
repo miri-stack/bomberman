4712793ab8b2a329cea7db5887898033c49e1adc
miri-stack <miriam.kaden@web.de>
2023-09-21 19:06:41 +0200

    Model hopefully ready

--- agent_code/mary_shelley/callbacks.py
+++ agent_code/mary_shelley/callbacks.py
@@ -1,17 +1,121 @@
 import numpy as np
-import pickle
+import pickle, os
 import events as e
 
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
+# Define rotation of movements based on agent position in the beginning
+def get_movement_lookup(position):
+    if position[0] != 1 and position[1] == 1:
+        dir_lookup_RO = {'UP':'RIGHT',
+                        'RIGHT':'DOWN',
+                        'DOWN': 'LEFT',
+                        'LEFT': 'UP',
+                        'BOMB': 'BOMB',
+                        'WAIT': 'WAIT'}
+        return dir_lookup_RO
+    
+    elif position[0] == 1 and position[1] != 1:
+        dir_lookup_LU = {'UP':'LEFT',
+                        'RIGHT':'UP',
+                        'DOWN': 'RIGHT',
+                        'LEFT': 'DOWN',
+                        'BOMB': 'BOMB',
+                        'WAIT': 'WAIT'}
+        return dir_lookup_LU
+    
+    elif position[0] != 1 and position[1] != 1:
+        dir_lookup_RU = {'UP':'DOWN',
+                        'RIGHT':'LEFT',
+                        'DOWN': 'UP',
+                        'LEFT': 'RIGHT',
+                        'BOMB': 'BOMB',
+                        'WAIT': 'WAIT'}
+        return dir_lookup_RU
+    
+    elif position[0] == 1 and position[1] == 1:
+        dir_lookup_LO = {'UP':'UP',
+                        'RIGHT':'RIGHT',
+                        'DOWN': 'DOWN',
+                        'LEFT': 'UP',
+                        'BOMB': 'BOMB',
+                        'WAIT': 'WAIT'}
+        return dir_lookup_LO
+
+# Rotate map based on startposition of agent
+def rotate_map(game_state_val, position):
+    """" Coordinate based values:
+        - field
+        - bombs
+        - explosion map
+        - coins
+        - self
+        - others
+    """
+    game_state = copy.deepcopy(game_state_val)
+    field_size = len(game_state['field'])-1 # Assume the map is quadratic
+    
+    if position == (1,1): # Remove later and make sure that function is only called in other cases
+        return game_state
+    
+    elif position[0] != 1 and position[1] != 1:
+        # Map based values
+        game_state['field'] = [list(reversed(row)) for row in reversed(game_state['field'])]
+        game_state['explosion_map'] = [list(reversed(row)) for row in reversed(game_state['explosion_map'])]
+
+        # Coordinate based values
+        game_state['bombs'] = [((field_size-o[0][0], field_size-o[0][1]), o[1]) for o in game_state['bombs']]
+        game_state['coins'] = [(field_size-coord[0], field_size-coord[1]) for coord in game_state['coins']]
+        game_state['self'] = (game_state['self'][0], game_state['self'][1], game_state['self'][2], (field_size-game_state['self'][3][0], field_size-game_state['self'][3][1]))
+        game_state['others'] = [(o[0], o[1], o[2], (field_size-o[3][0], field_size-o[3][1])) for o in game_state['others']]
+
+        return game_state
+
+
+    elif position[0] == 1 and position[1] != 1:
+        # Map based values
+        game_state['field'] = list(reversed(list(map(list, zip(*game_state['field'])))))
+        game_state['explosion_map'] = list(reversed(list(map(list, zip(*game_state['explosion_map'])))))
+
+        # Coordinate based values
+        # (x,y) -> y = x, x = len-y
+        game_state['bombs'] = [((field_size-o[0][1], o[0][0]), o[1]) for o in game_state['bombs']]
+        game_state['coins'] = [(field_size-coord[1], coord[0]) for coord in game_state['coins']]
+        game_state['self'] = (game_state['self'][0], game_state['self'][1], game_state['self'][2], (field_size-game_state['self'][3][1], game_state['self'][3][0]))
+        game_state['others'] = [(o[0], o[1], o[2], (field_size-o[3][1], o[3][0])) for o in game_state['others']]
+
+        return game_state
+    
+    elif position[0] != 1 and position[1] == 1:
+        
+        # Map based values
+        game_state['field'] = [list(reversed(row)) for row in zip(*game_state['field'])]
+        game_state['explosion_map'] = [list(reversed(row)) for row in zip(*game_state['explosion_map'])]
+
+        # Coordinate based values
+        # (x,y) -> y = len-x, x = y
+        game_state['bombs'] = [((o[0][1], field_size-o[0][0]), o[1]) for o in game_state['bombs']]
+        game_state['coins'] = [(coord[1], field_size-coord[0]) for coord in game_state['coins']]
+        game_state['self'] = (game_state['self'][0], game_state['self'][1], game_state['self'][2], (game_state['self'][3][1], field_size-game_state['self'][3][0]))
+        game_state['others'] = [(o[0], o[1], o[2], (o[3][1], field_size-o[3][0])) for o in game_state['others']]
+
+        return game_state
+
 def setup(self):
     self.logger.info('Mary Shelly is getting ready.')
-    if self.train:
-        
+    self.current_round = 0
+    self.orientation = 0
+
+    if self.train and not os.path.isfile('my-saved-model.pt'):
+        self.logger.info('Setting up model from scratch.')
+        self.qtable = {}
     else:
-        with open(qtable_load, "rb") as file:
+        self.logger.info('Loading model from saved state.')
+        with open('my-saved-model.pt', 'rb') as file:
             self.qtable = pickle.load(file)
+
+
     # initialize Q(S,A) to 0 for all S and A
     # Start Field 
 
@@ -20,6 +124,101 @@ def act(self, game_state: dict):
     self.logger.info('Pick action according to pressed key')
     # observe environment
 
+    if game_state['step'] == 1: # First round, setup orientation
+        self.orientation = game_state['self'][3]
+        self.lookup_move = get_movement_lookup(game_state['self'][3])
+
+    # Rotate Map
+    game_state = rotate_map(game_state, self.orientation)
+
+    current_orientation = game_state['self'][3]
+    # Check if orientation correct
+    self.logger.debug(f'The orientation should be (1,1) and is {current_orientation}')
+
+    random_number = random.uniform(0,1)
+    if random_number > epsilon:
+        action = np.argmax(qtable[state])
+    else:
+        # choose random action
+        self.action = np.random.choice(self.actions)
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    x_self, y_self = game_state['self'][3]
+    bombs = game_state['bombs']
+    coins = game_state['coins']
+    explosions = game_state['explosion_map']
+    field = game_state['field']
+    others = game_state['others']
+
+    view = []
+    countdowns = []
+    directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
+    for direct in directions:
+        x_dir, y_dir = direct
+        x, y = x_self + x_dir, y_self + y_dir
+        tile_info = field[x,y]
+        if x_dir < 0 or y_dir < 0 or x_dir >= len(field) or y_dir >= len(field[0]):
+                # Out of bounds, consider it a wall
+                view.append(-2)
+        elif tile_info == 0:
+            for bomb in bombs:
+                if bomb[0][0] == x and bomb[0][1] == y: view.append(-2)
+                # explosion
+            for coin in coins:
+                if coin[0] == x and coin[1] == y: view.append(2)
+        else:
+            view.append(tile_info)
+
+        # Check if the tile is within the bounds of the explosion map.
+        if 0 <= x < explosion_map.shape[0] and 0 <= y < explosion_map.shape[1]:
+            countdown = explosion_map[x][y]
+            normalized_countdown = countdown / 3.0  # Normalize by dividing by the maximum countdown (3)
+            countdowns.append(normalized_countdown)
+        else:
+            countdowns.append(0)  # If out of bounds, countdown is 0.
+
+    # Manhatten Distance to nearest coin
+    nearest_coin = float('inf')
+    for coin_pos in coins:
+        distance = abs(x_self - coin_pos[0]) + abs(y_self - coin_pos[1])
+        if distance < nearest_coin:
+            nearest_coin = distance
+
+    # Manhatten Distance to nearest bomb
+    nearest_bomb = float('inf')
+    for bomb_pos in bombs:
+        distance = abs(x_self - bomb_pos[0]) + abs(y_self - bomb_pos[1])
+        if distance < nearest_bomb:
+            nearest_bomb = distance
+
+    # Manhatten Distance to nearest opponent
+    nearest_opponent = float('inf')
+    for other in others:
+        distance = abs(x_self - other[3][0]) + abs(y_self - other[3][1])
+        if distance < nearest_opponent:
+            nearest_opponent = distance
+
+    return view + countdowns + nearest_coin + nearest_bomb + nearest_opponent
+        
+
     
-    # return game_state['user_input']
+
 
--- agent_code/mary_shelley/train.py
+++ agent_code/mary_shelley/train.py
@@ -1,7 +1,14 @@
 import events as e
+import pickle, os
 
 # define parameters
 # alpha, gamma epsilon
+ALPHA = 0.001
+GAMMA = 0.99
+EPSILON = 0.9
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 # update action value funtion
 # action selection: (epsilon) greedy for exploration decrease after n episodes 
@@ -10,27 +17,76 @@ def setup_training(self):
 ''' called when loading the agent, after calling setup in callbacks.py.
 Use this to initialize variables you only need for training.
 '''
-    self.qtable = pd.DataFrame(columns=ACTIONS,dtype=np.float64)
+  self.statistic_dict = None
+  self.gamma = self.GAMMA
+  self.epsilon = self.EPSILON
+
+  # self.state_dim = hp.INPUT_SHAPE[0]*hp.INPUT_SHAPE[1]
+  self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+  # self.qtable = pd.DataFrame(columns=ACTIONS,dtype=np.float64)
 
 
-def game_events_occurred(self, old_game_state, self_action, new_game_state, events):
-''' is called once after each step except the last. At this point, all the actions have been executed and their consequences are known. Use this callback to collect training data and ll an
-experience buffer '''
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    # Setup statistics dictionary if necessary
+    if old_game_state['step'] == 1: # Initialise new dict
+        self.statistic_dict = defaultdict(int) # defaultdict with 0 as default value
+        self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE) # Reset transitions
+
+
+    # Decrease exploration linearly
+    if old_game_state['round'] % 100 == 0 and self.epsilon > 0.1:
+        self.epsilon -= 0.1
+    # Idea: Add your own events to hand out rewards
+
+    # Get old_game_state
+    old_game_state_adjusted = rotate_map(old_game_state, self.orientation)
+    old_state = state_to_features(old_game_state_adjusted)
+  
+    # Get new_game_state
+    new_game_state_adjusted = rotate_map(new_game_state, self.orientation)
+    new_state = state_to_features(new_game_state_adjusted)
+
     reward = get_reward_from_events(events)
-    qtable[old_game_state, self_action] = qtable[old_game_state, self_action] + self.alpha * (reward + self.gamma * (qtable[new_game_state,self_action] - qtable[old_game_state,self_action])
+    self.transitions.append(Transition(old_game_state, self_action, new_game_state, reward))
 
+    last_transition = transitions[-1]
+
+    if last_transition.state not in self.qtable:
+      self.qtable[last_transition.state] = {}
+    if not self.qtable[old_game_state][action]:
+       self.qtable[old_game_state]
+    self.qtable[last_transition.state][last_transition.action] = qtable[last_transition.state, last_transition.action] + self.alpha * (last_transition.reward + self.gamma * (self.qtable[old_game_state,self_action] - self.qtable[last_transition.state,last_transition.action])
+
+
+    
 
 def end_of_round(self, last_game_state, last_action, events):
 '''is very similar to the previous, but only called once per agent after the last step of a round '''
+  # Store the model in every 10th episode
+  if last_game_state['round'] % 10 == 0:
+      with open("save_files/table_" + str(last_game_state['round']) + ".pt","w") as file:
+            pickle.dump(self.Q, file)
 
-def epsilon_greedy_policy(self, qtable, state, epsilon):
-  random_number = random.uniform(0,1)
-  if random_number > epsilon:
-    action = np.argmax(qtable[state])
-  else:
-    # choose random action
-    action = 
-  return action
+
+
 
 def get_reward_from_events(self, events) -> int:
         game_rewards = {
@@ -41,7 +97,7 @@ def get_reward_from_events(self, events) -> int:
             e.MOVED_UP: 1,
             e.MOVED_DOWN: 1,
             e.WAITED: -1,
-            e.INVALID_ACTION: -10,
+            e.INVALID_ACTION: -200,
             e.BOMB_EXPLODED: 50,
             e.CRATE_DESTROYED: 100,
             e.COIN_FOUND: 100,
@@ -54,5 +110,5 @@ def get_reward_from_events(self, events) -> int:
              reward_sum += game_rewards[event]
         self.logger("Rewards granted for this round")
         # table or something for those
-        return reward_sum
+      return reward_sum
 
